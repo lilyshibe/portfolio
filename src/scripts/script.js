edfdf65b826a9a634f686ecf6fc07e0ff3a9ca0a
lillian rose winter <lily@null.net>
2019-06-08 04:36:48 -0500

    :bug: i'm really trying to make this work

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -157,24 +157,24 @@ $(function() {
     $('body').bind('unsnap', function(e) {
         unsnap();
     });
+});
 
-    let clientX = -100;
-    let clientY = -100;
-    const innerCursor = document.querySelector(".cursor");
+let clientX = -100;
+let clientY = -100;
+const innerCursor = document.querySelector(".cursor");
 
-    const initCursor = () => {
-        document.addEventListener("mousemove", e => {
-            clientX = e.clientX;
-            clientY = e.clientY;
-        });
+const initCursor = () => {
+    document.addEventListener("mousemove", e => {
+        clientX = e.clientX;
+        clientY = e.clientY;
+    });
 
-        const render = () => {
-            innerCursor.style.transform = `translate: (${clientX}px, ${clientY}px)`;
-            requestAnimationFrame(render);
-        };
-
+    const render = () => {
+        innerCursor.style.transform = `translate: (${clientX}px, ${clientY}px)`;
         requestAnimationFrame(render);
     };
 
-    initCursor();
-});
+    requestAnimationFrame(render);
+};
+
+initCursor();
