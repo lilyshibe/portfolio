544982a2559d28f42f0cd814b2b3d37d171fbc9b
lillian rose winter <lily@null.net>
2019-06-08 04:48:28 -0500

    :bug: it works

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -161,15 +161,17 @@ $(function() {
 
 let clientX = -100;
 let clientY = -100;
-const innerCursor = document.querySelector(".cursor");
+const innerCursor = document.querySelector(".cursor--small");
 
 document.addEventListener("mousemove", e => {
     clientX = e.clientX;
     clientY = e.clientY;
+    // console.log(clientX);
+    // console.log(clientY);
 });
 
 const render = () => {
-    innerCursor.style.transform = `translate: (${clientX}px, ${clientY}px)`;
+    innerCursor.style.transform = `translate(${clientX}px, ${clientY}px)`;
     requestAnimationFrame(render);
 };
 
