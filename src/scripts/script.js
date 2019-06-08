2f1d5d363044e0a0f78477a6001d2b7b3519e68d
lillian rose winter <lily@null.net>
2019-06-08 04:39:08 -0500

    :bug: PLEASE JUST WORK

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -163,18 +163,14 @@ let clientX = -100;
 let clientY = -100;
 const innerCursor = document.querySelector(".cursor");
 
-const initCursor = () => {
-    document.addEventListener("mousemove", e => {
-        clientX = e.clientX;
-        clientY = e.clientY;
-    });
+document.addEventListener("mousemove", e => {
+    clientX = e.clientX;
+    clientY = e.clientY;
+});
 
-    const render = () => {
-        innerCursor.style.transform = `translate: (${clientX}px, ${clientY}px)`;
-        requestAnimationFrame(render);
-    };
-
+const render = () => {
+    innerCursor.style.transform = `translate: (${clientX}px, ${clientY}px)`;
     requestAnimationFrame(render);
 };
 
-initCursor();
+requestAnimationFrame(render);
