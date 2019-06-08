a3db73d23a64d95082a4f0c7cb86544230a65459
lillian rose winter <lily@null.net>
2019-06-08 04:25:57 -0500

    :sparkles: make small cursor JS based
    low quality image was pissing me off

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -157,4 +157,24 @@ $(function() {
     $('body').bind('unsnap', function(e) {
         unsnap();
     });
+
+    let clientX = -100;
+    let clientY = -100;
+    const innerCursor = document.querySelector(".cursor");
+
+    const initCursor = () => {
+        document.addEventListener("mousemove", e => {
+            clientX = e.clientX;
+            clientY = e.clientY;
+        });
+
+        const render = () => {
+            innerCursor.style.transform = `translate: (${clientX}px, ${clientY}px)`;
+            requestAnimationFrame(render);
+        };
+
+        requestAnimationFrame(render);
+    };
+
+    initCursor();
 });
